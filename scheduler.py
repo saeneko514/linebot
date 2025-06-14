507b1f33b6a14217bd7176a9a94fe841010f1926
Saeko <saeko514@outlook.jp>
2025-06-14 14:30:46 +0900

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -3,7 +3,7 @@ import time
 from job_project_a import job_project_a
 
 # スケジュール設定（日本時間-9時間）
-schedule.every().day.at("02:45").do(job_project_a)
+schedule.every().day.at("05:35").do(job_project_a)
 
 if __name__ == "__main__":
     print("Scheduler started.")
