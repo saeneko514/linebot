480771436c5f7e95f0d02fb79ff0ffb0eda273d0
Saeko <saeko514@outlook.jp>
2025-06-14 15:53:27 +0900

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -4,7 +4,7 @@ from job_project_a import job_project_a
 from job_project_b import job_project_b
 
 # スケジュール設定（日本時間-9時間）
-schedule.every().day.at("06:50").do(job_project_a)
+schedule.every().day.at("06:55").do(job_project_a)
 schedule.every().day.at("06:21").do(job_project_b)
 
 if __name__ == "__main__":
